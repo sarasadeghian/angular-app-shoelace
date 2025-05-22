ea064396956a291a4aefd4bf4c7926902babe9cf
Sara Sadeghian <sara.sadeghian.t@gmail.com>
2025-05-22 13:28:43 +0200

    feat(main): adding shoelace to the project

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@
  *  Protractor support is deprecated in Angular.
  *  Protractor is used in this example for compatibility with Angular documentation tools.
  */
-import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import routeConfig  from './app/routes';
 import { AppComponent } from './app/app.component';
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
 
-bootstrapApplication(AppComponent, {providers: [provideProtractorTestingSupport(), provideRouter(routeConfig)]}).catch((err) =>
+setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.20.1/cdn/');
+
+
+bootstrapApplication(AppComponent, {providers: [provideRouter(routeConfig)]}).catch((err) =>
   console.error(err),
 );

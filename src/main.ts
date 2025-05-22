9449175164e435200ba189ac42a4fb5d9f3eb014
Sara Sadeghian <sara.sadeghian.t@gmail.com>
2025-05-22 13:12:09 +0200

    feat(main): create a regular angular app

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+/*
+ *  Protractor support is deprecated in Angular.
+ *  Protractor is used in this example for compatibility with Angular documentation tools.
+ */
+import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import routeConfig  from './app/routes';
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent, {providers: [provideProtractorTestingSupport(), provideRouter(routeConfig)]}).catch((err) =>
+  console.error(err),
+);

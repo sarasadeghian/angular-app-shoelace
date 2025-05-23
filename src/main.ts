ce92960f909699d70603f0a84d1149de27924ddf
Sara Sadeghian <sara.sadeghian.t@gmail.com>
2025-05-23 14:37:55 +0200

    adjust some settings to use shoelace

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
-/*
- *  Protractor support is deprecated in Angular.
- *  Protractor is used in this example for compatibility with Angular documentation tools.
- */
+import '@shoelace-style/shoelace/dist/components/icon/icon.js';
+import '@shoelace-style/shoelace/dist/components/icon/library.js';
+import { registerIconLibrary } from '@shoelace-style/shoelace/dist/utilities/icon-library.js';
+
+registerIconLibrary('default', {
+  resolver: name =>
+    `https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/icons/${name}.svg`,
+  mutator: svg => svg.setAttribute('fill', 'currentColor')
+});
+
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
+setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.20.1/cdn/');
+
+
+import '@shoelace-style/shoelace/dist/components/input/input.js';
+import '@shoelace-style/shoelace/dist/components/button/button.js';
+import '@shoelace-style/shoelace/dist/components/icon/icon.js';
+
 import { bootstrapApplication } from '@angular/platform-browser';
-import { CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import routeConfig  from './app/routes';
 import { AppComponent } from './app/app.component';
-import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
-
-setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.20.1/cdn/');
 
 
 bootstrapApplication(AppComponent, {providers: [provideRouter(routeConfig)]}).catch((err) =>
